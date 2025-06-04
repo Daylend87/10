d21948b76215dae3e6ae32cc650d6ab858385c06
Daylend87 <macha05112011@gmail.com>
2025-06-04 23:25:57 +0500

    Inputoutput.cs

--- Inputoutput.cs
+++ Inputoutput.cs
@@ -1,121 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Компилятор
 {
-    class InputOutput
+    class Program
     {
-        private const byte ERRMAX = 9;
-        private static char Ch { get; set; }
-        private static TextPosition positionNow = new TextPosition();
-        private static List<string> lines;
-        private static int currentLineIndex = -1;
-        private static List<Err> err;
-
-        private static readonly Dictionary<byte, string> errorDescriptions = new Dictionary<byte, string>
+        static void Main()
         {
-            { 1, "Отсутствует точка с запятой (;)" },
-            { 2, "Неизвестное имя переменной" },
-            { 3, "Несовместимость типов при присваивании" },
-            { 4, "Переменная не инициализирована" },
-            { 5, "Отсутствует закрывающий апостроф" }
-        };
+            string testFile = "Test.pas";
+            ProcessFile(testFile);
+        }
 
-        public static void Initialize(string filePath)
+        static void ProcessFile(string filePath)
         {
-            lines = new List<string>();
-            err = new List<Err>();
-            try
+            InputOutput.Initialize(filePath);
+            SimulateErrors(filePath);
+            List<string> lines = InputOutput.GetLines();
+            for (uint lineNum = 1; lineNum <= lines.Count; lineNum++)
             {
-                using (StreamReader file = new StreamReader(filePath))
+                Console.WriteLine(lines[(int)lineNum - 1]);
+                List<Err> errorsInLine = InputOutput.GetErrorsForLine(lineNum);
+                foreach (var err in errorsInLine)
                 {
-                    string line;
-                    while ((line = file.ReadLine()) != null)
-                    {
-                        lines.Add(line);
-                    }
-                }
-                if (lines.Count > 0)
-                {
-                    currentLineIndex = 0;
-                    Ch = lines[0].Length > 0 ? lines[0][0] : '\0';
+                    string errorLine = InputOutput.GetErrorLine(err);
+                    if (!string.IsNullOrEmpty(errorLine))
+                        Console.WriteLine(errorLine);
                 }
             }
-            catch (Exception)
+
+            InputOutput.ClearErrors();
+        }
+
+        static void SimulateErrors(string filePath)
+        {
+            if (filePath == "Test.pas")
             {
+                InputOutput.Error(1, new TextPosition(8, 9));  
+                InputOutput.Error(2, new TextPosition(9, 3));  
+                InputOutput.Error(3, new TextPosition(12, 8)); 
+                InputOutput.Error(4, new TextPosition(14, 8)); 
+                InputOutput.Error(5, new TextPosition(16, 18)); 
             }
-        }
-
-        public static void NextCh()
-        {
-            if (currentLineIndex < 0 || currentLineIndex >= lines.Count)
-            {
-                return;
-            }
-
-            string currentLine = lines[currentLineIndex];
-            if (positionNow.CharNumber == (currentLine?.Length ?? 0) - 1)
-            {
-                positionNow.LineNumber = (uint)(currentLineIndex + 1);
-                positionNow.CharNumber = 0;
-                currentLineIndex++;
-                if (currentLineIndex < lines.Count)
-                {
-                    Ch = lines[currentLineIndex].Length > 0 ? lines[currentLineIndex][0] : '\0';
-                }
-                else
-                {
-                    Ch = '\0';
-                }
-            }
-            else if (positionNow.CharNumber < (currentLine?.Length ?? 0) - 1)
-            {
-                positionNow.CharNumber++;
-                Ch = currentLine[positionNow.CharNumber];
-            }
-            else
-            {
-                Ch = '\0';
-            }
-        }
-
-        public static List<string> GetLines()
-        {
-            return new List<string>(lines);
-        }
-
-        public static string GetErrorLine(Err item)
-        {
-            if (lines == null || item.ErrorPosition.LineNumber == 0 || item.ErrorPosition.LineNumber > lines.Count) return "";
-            int charPosition = item.ErrorPosition.CharNumber - 1;
-            if (charPosition < 0) charPosition = 0;
-            string spaces = new string(' ', charPosition);
-            string errorMessage = $"^ ошибка код {item.ErrorCode} ({errorDescriptions[item.ErrorCode]})";
-            return spaces + errorMessage;
-        }
-
-        public static List<Err> GetErrorsForLine(uint lineNumber)
-        {
-            return err.FindAll(e => e.ErrorPosition.LineNumber == lineNumber);
-        }
-
-        public static void ClearErrors()
-        {
-            err.Clear();
-        }
-
-        public static void Error(byte errorCode, TextPosition position)
-        {
-            if (err.Count < ERRMAX)
-            {
-                err.Add(new Err(position, errorCode));
-            }
-        }
-
-        public static bool EndOfFile()
-        {
-            return currentLineIndex >= lines.Count;
         }
     }
 }
